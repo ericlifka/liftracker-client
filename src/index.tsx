9bcbeaa3f50ce02fe72b30292b6b846b24c1ed6f
Eric Lifka <eric.lifka@lifeomic.com>
2020-10-25 23:11:25 -0400

    bring in react router tutorial for authed routes to build from

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from "react-redux"
 
-import App from './components/App'
+import { App } from './components/App'
 import { configureAppStore }  from "./configure-store"
 import './index.css'
 
